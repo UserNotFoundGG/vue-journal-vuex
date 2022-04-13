178c79a5ba8b05d95e0be3c007c9c91c08a07db7
Nobody <efra123d456@gmail.com>
2022-04-13 15:07:11 -0400

    Primer commit

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(), //12371298314862
+            date: new Date().toDateString(),  // sat 23, julio
+            text: 'Incididunt commodo occaecat do do mollit culpa elit pariatur esse est cillum excepteur culpa.',
+            picture: null   // https://
+        },
+        {
+            id: new Date().getTime() + 1000, //12371298314862
+            date: new Date().toDateString(),  // sat 23, julio
+            text: 'Cillum ea qui incididunt proident elit culpa velit ipsum tempor aliquip labore nisi ullamco dolor.',
+            picture: null   // https://
+        },
+        {
+            id: new Date().getTime() + 2000, //12371298314862
+            date: new Date().toDateString(),  // sat 23, julio
+            text: 'Eu enim sint ad ad Lorem ullamco laborum et excepteur exercitation aliqua elit veniam incididunt.',
+            picture: null   // https://
+        },
+    ]
+})
+
